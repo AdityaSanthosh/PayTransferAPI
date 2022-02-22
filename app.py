46590a1f092e992c32ffe21cb6cb859deabd5b53
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2022-02-22 19:29:43 +0530

    Added Error Handling. Added Error messages to users

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import decimal
-
 from flask import Flask
 from flask import request
 import psycopg2
 import json
-from time import time
 
 
 class DecimalEncoder(json.JSONEncoder):
@@ -25,12 +23,28 @@ cur = conn.cursor()
 def deposit():
     assert request.path == '/deposit'
     assert request.method == 'POST'
-    data = json.loads(request.data)
-    account = data['from']
-    amount = Decimal(data["amount"])
-    balance = None
-    # not finished. example script already present in deposit.py
-    return json.dumps({'account': account, 'balance': balance, 'message': 'success/failed'})
+    try:
+        data = json.loads(request.data)
+    except json.JSONDecodeError:
+        return json.dumps({'message': 'Please Enter the Input in the JSON format correctly'})
+    try:
+        assert len(data) == 2
+    except AssertionError:
+        return json.dumps({'message': 'Remove any additional fields other than account and amount'})
+    try:
+        account = data['account']
+        amount = int(data["amount"])
+    except KeyError:
+        return json.dumps({'message': 'Please Enter the Input in 2 fields. account and amount. both strings'})
+    try:
+        cur.execute("UPDATE balances SET balance = balance + %s WHERE account_no = %s;", (amount, account))
+        conn.commit()
+    except psycopg2.Error as e:
+        conn.rollback()
+        print(e.pgerror)
+    cur.execute("SELECT balance FROM balances WHERE account_no=%s;", (account,))
+    balance = cur.fetchone()
+    return json.dumps({'account': account, 'balance': balance, 'message': 'success'}, cls=DecimalEncoder)
 
 
 @app.route('/transfer', methods=['POST'])
@@ -40,11 +54,57 @@ def transfer():
     # user.ongoingtransaction = True
     assert request.path == '/transfer'
     assert request.method == 'POST'
-    data = json.loads(request.data)
+    try:
+        data = json.loads(request.data)
+    except json.JSONDecodeError:
+        return json.dumps({'message': 'Please Enter the Input in the JSON format correctly'})
     from_account = data['from']
     to_account = data["to"]
-    amount = int(data["amount"])
-    cur.execute("call transact(%s,%s,%s,null);", (from_account, to_account, amount))
+    try:
+        amount = int(data["amount"])
+    except ValueError:
+        return json.dumps({'message': 'Please Enter a Number for input'})
+    try:
+        assert len(data) == 3
+    except AssertionError:
+        return json.dumps({'message': 'Remove any additional fields other than from_account, to_account and amount'})
+    try:
+        assert amount > 0
+    except AssertionError:
+        return json.dumps({'message': 'Please Enter Positive Values of Amount'})
+    errormsg = "Oops! Something wrong on our servers. Don't worry. Your transaction is already rolled back."
+    try:
+        cur.execute("call transact(%s,%s,%s,null);", (from_account, to_account, amount))
+    except psycopg2.IntegrityError as e:
+        conn.rollback()
+        if e.pgcode == '23514':
+            return json.dumps({'message': "Balance Insufficient"})
+        if e.pgcode == '23503':
+            return json.dumps({'message': "No Such Accounts present in the System. Please Check if you entered the "
+                                          "proper Account Number"})
+        else:
+            print(e.pgcode, e.pgerror)
+            return json.dumps({"message": errormsg})
+    except psycopg2.OperationalError as e:
+        conn.rollback()
+        print(e.pgcode, e.pgerror)
+        return json.dumps({"message": errormsg})
+    except psycopg2.InternalError as e:
+        conn.rollback()
+        print(e.pgcode, e.pgerror)
+        return json.dumps({"message": errormsg})
+    except psycopg2.ProgrammingError as e:
+        conn.rollback()
+        print(e.pgcode, e.pgerror)
+        return json.dumps({"message": errormsg})
+    except psycopg2.DatabaseError as e:
+        conn.rollback()
+        print(e.pgcode, e.pgerror)
+        return json.dumps({"message": errormsg})
+    except psycopg2.Error as e:
+        conn.rollback()
+        print(e.pgerror, e.pgcode)
+        return json.dumps({"message": errormsg})
     """ This one works too...
     cur.execute("create or replace function transact(fromacc varchar,toacc varchar, amt numeric) RETURNS uuid as $$ "
                 "update balances set balance = balance - amt where account_no = fromacc;"
@@ -77,7 +137,8 @@ def transfer():
             "balance": to_balance
         },
         "transfered": amount,
-        "created_datetime": created_at
+        "created_datetime": created_at,
+        "message": "Success"
     }
     # user.ongoingtransaction = False
     return json.dumps(response, cls=DecimalEncoder, default=str)
