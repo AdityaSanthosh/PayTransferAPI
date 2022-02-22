7b6598248f2f4e3075669fb3b7180620165f1b44
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2022-02-22 20:54:00 +0530

    Facilitated Ease of setup, updated instructions

--- readme.md
+++ readme.md
@@ -2,13 +2,20 @@
 1. Download Postgres, an API testing tool (I used Insomnia). Install them
 2. `git clone` this repo. Create a virtual environment.  Install the packages in `requirements.txt` file
 3. Spin up a new server and a new postgres database. Replace the database configuration present in the `app.py` file with that of your database
-4. Run the instructions present in the `setup.py` in psql. I has configuration to create database tables, add configuration and initial data and the code in `deposit.py` to deposit some data into the tables for working.
+4. Run the instructions present in the `setup.py` in psql. It has the configuration to create database tables, add checks and constraints. Deposit some money at `/deposit` path to facilitate transfers.
 5.  Run `flask run` after exporting the env variables. Check the official flask docs if necessary.
 6. Send a post request to `localhost:5000` (flask port) in json format with the input format mentioned below.
 7. You can see the output in the insomnia/ postman terminal.
 
 ### Input format:
 
+##### _Deposit_
+
+`{ "from": "account_no",
+    "to": "account_no",
+    "amount": "money"}`
+
+##### _Transfer_
 `{ "from": "account_no",
     "to": "account_no",
     "amount": "money"}`
@@ -41,7 +48,7 @@ _**I tried to implement most of the logic in SQL as stored procedures. This is d
 
 Other Technical details are present in `considerations.txt` file in the repository
 
-###### _The Important part of the code for the impatient_
+##### _The Important part of the code for the impatient_
 
 ```
 CREATE or replace PROCEDURE transact(fromacc varchar, toacc varchar, amt numeric, out transactionid uuid)
--- scripts/setup.py
+++ scripts/setup.py
@@ -1,6 +1,10 @@
 import psycopg2
+
 # Insert your db config here
-conn = psycopg2.connect("dbname=transactionsdb user=postgres host=localhost password=postgres")
+try:
+    conn = psycopg2.connect("dbname=transactionsd user=postgres host=localhost password=postgres")
+except psycopg2.OperationalError as e:
+    raise
 cur = conn.cursor()
 
 # python Model
@@ -22,28 +26,39 @@ TABLE account_type
 """
 Creating Tables
 """
-cur.execute("CREATE TABLE IF NOT EXISTS transactions"
-            "(id uuid PRIMARY KEY DEFAULT gen_random_uuid (),"
-            "amount numeric(15,4),"
-            "credit_account_no VARCHAR(10) FOREIGN KEY(account_no) REFERENCES balances(account_no) ON DELETE RESTRICT,"
-            "debit_account_no VARCHAR(10) FOREIGN KEY(account_no) REFERENCES balances(account_no) ON DELETE RESTRICT,"
-            "created_datetime timestamp NOT NULL set default current_timestamp);")
+
+try:
+    cur.execute("CREATE TABLE IF NOT EXISTS transactions"
+                "(id uuid PRIMARY KEY DEFAULT gen_random_uuid (),"
+                "amount numeric(15,4),"
+                "credit_account_no VARCHAR(10) FOREIGN KEY(account_no) REFERENCES balances(account_no) ON DELETE RESTRICT,"
+                "debit_account_no VARCHAR(10) FOREIGN KEY(account_no) REFERENCES balances(account_no) ON DELETE RESTRICT,"
+                "created_datetime timestamp NOT NULL set default current_timestamp);")
+except psycopg2.Error:
+    raise
+
 # not null is not really needed when the values are automatically generated but I left it there for intuition
 
-cur.execute("CREATE TABLE IF NOT EXISTS balances"
-            "balance numeric(15,4) NOT NULL,"
-            "account_no VARCHAR(10) NOT NULL unique);")
+try:
+    cur.execute("CREATE TABLE IF NOT EXISTS balances"
+                "balance numeric(15,4) NOT NULL,"
+                "account_no VARCHAR(10) NOT NULL unique);")
+except psycopg2.Error:
+    raise
 
-"""
-Creating Indices for faster selects
-"""
-cur.execute("CREATE INDEX account_no_lookup ON balances(account_no);")
+# """
+# Creating Indices for faster selects
+# """
 
-cur.execute("create index account_no_lookup on balances (account_no);")
+try:
+    cur.execute("create index account_no_lookup on balances (account_no);")
+except psycopg2.Error:
+    raise
 
-"""
-Some Constraints which are important for consistency
-"""
+# """
+# Some Constraints which are important for consistency
+# """
+
 cur.execute("GRANT ALL PRIVILEGES ON DATABASE transactionsdb TO aditya;")
 cur.execute("ALTER ROLE aditya SET CLIENT_ENCODING TO 'utf-8';")
 cur.execute("ALTER ROLE aditya SET default_transaction_isolation TO 'read committed';")
@@ -51,28 +66,19 @@ cur.execute("ALTER ROLE aditya SET TIMEZONE TO 'UTC';")
 cur.execute("ALTER TABLE balances ADD CONSTRAINT Check_minimumBalance CHECK (balance >= 100);")
 cur.execute("CREATE RULE transactions_delete AS ON DELETE TO transactions DO INSTEAD NOTHING;")
 
-# Please run the following procedure in psql tool.
-"""
-CREATE or replace PROCEDURE transact(fromacc varchar, toacc varchar, amt numeric, out transactionid uuid)
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    update balances
-            set balance = balance - amt
-            where account_no = fromacc;
-            update balances
-            set balance = balance + amt
-            where account_no = toacc;
-            INSERT INTO transactions(amount, credit_account_no, debit_account_no) VALUES (amt, toacc, fromacc)
-            returning id into transactionid;
-END;
-$$;
-"""
+try:
+    cur.execute(
+        "CREATE or replace PROCEDURE transact(fromacc varchar, toacc varchar, amt numeric, out transactionid uuid) "
+        "LANGUAGE plpgsql AS $$ "
+        "BEGIN update balances "
+        "set balance = balance - amt where account_no = fromacc;"
+        " update balances set balance = balance + amt where account_no = toacc; "
+        "INSERT INTO transactions(amount, credit_account_no, debit_account_no) VALUES (amt, toacc, fromacc) returning id into transactionid; "
+        "END;"
+        "$$;")
+except psycopg2.Error as e:
+    raise
 
 conn.commit()
 cur.close()
 conn.close()
-
-
-
-
